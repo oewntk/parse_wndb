cff45c69f11cb7065d0754f267660ce3f2ca0c1e
1313ou <1313ou@gmail.com>
2026-07-21 07:22:15 +0200

    make test correct and meaningful

--- src/test/java/org/oewntk/parse/TestDecode.kt
+++ src/test/java/org/oewntk/parse/TestDecode.kt
@@ -2,15 +2,16 @@ package org.oewntk.parse
 
 import org.oewntk.utils.Tracing
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class TestDecode {
 
     @Test
-    fun TextDecode() {
+    fun textDecode() {
         val rawLine = "usually followed by âtoâ) having the necessary means"
         val byteArray = rawLine.toByteArray(Charsets.ISO_8859_1)
         val line = String(byteArray, Flags.charSet)
-        Tracing.psInfo.println(rawLine)
-        Tracing.psInfo.println(line)
+        Tracing.psInfo.println("$rawLine -> $line")
+        assertEquals("usually followed by ‘to’) having the necessary means", line)
     }
 }
